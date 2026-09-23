bbbce8f28aad1e0011225810e22bb6b9e51114b1
Mohammad Riaz <mohammadriaz60000@gmail.com>
2026-09-24 01:22:28 +0600

    setting up project

---
+++ prisma7.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default defineConfig({
+  schema: "prisma/schema/",
+
+  migrations: {
+    path: "prisma/migrations",
+  },
+
+  datasource: {
+    url: process.env.DATABASE_URL!,
+  },
+});
